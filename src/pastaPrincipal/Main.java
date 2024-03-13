e8f5bf711e7623b9df14fac29e799ff4be8740ee
Besteres <andrerioslol@outlook.com>
2024-03-13 18:09:58 +0000

    Package postgres local + Mensagem de encerramento

--- src/pastaPrincipal/Main.java
+++ src/pastaPrincipal/Main.java
@@ -4,7 +4,13 @@ import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.Calendar;
+import java.util.Date;
+import java.sql.Timestamp;
 
 import sistema.BDDriver;
 import users.Autor;
@@ -16,8 +22,13 @@ import users.Utilizador;
 
 public class Main {
 	
+	//Eu nao queria estar a meter isto aqui
+	//Mas nao funciona de outra forma já que o programa termina fora da função main
+	static long startmillis;
+	static long endmillis;
+	
 	public static void main(String [] args)  {
-		
+		startmillis = System.currentTimeMillis();
 		
 		while(true) {
 			System.out.println("1-Registar \n2-Login \n3-Sair");
@@ -26,10 +37,8 @@ public class Main {
 			
 			executaOpcao(opcao);
 			
-		}
-			
-				
-		}
+		}		
+	}
 	
 	
 	private static void executaOpcao(int aOpcao){
@@ -49,7 +58,24 @@ public class Main {
 	private static void sair() {
 		//System.out.println("Adeus " + userLoginName);
 		System.out.println("A encerrar aplicação!"); 
+		endmillis = System.currentTimeMillis();
+		
+		PrintTimeStats(startmillis,endmillis);
 		System.exit(0);								
+	}
+	
+	private static void PrintTimeStats(long start, long end) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE; yyyy-MM-dd HH:mm:ss",Locale.forLanguageTag("Pt"));
+		
+		System.out.println("Início do processo: " + dateFormat.format(new Date(start)));
+		System.out.println("Fim do processo: " + dateFormat.format(new Date(end)));
+		long diff = (end - start);
+		int segundos = (int)(diff/1000) % 60; //Só existem 60 segundos num minuto
+		int minutos = (int)((diff/(1000*60)) % 60); //Só existem 60 minutos numa hora
+		int horas = (int)((diff/(1000*60*60)) % 24); //Só existem 24 horas diferentes
+		
+		System.out.println("Tempo de execução: " + diff + " Milissegundos "+
+		"("+segundos+ " Segundos; "+minutos+" Minutos; "+horas+ " Horas)");
 	}
 	
 	private static void registo() {
